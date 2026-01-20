88d20c8fca23a2f439c7bf5825a72d8f3c645611
meti2626 <metseboka@gmail.com>
2026-01-20 23:34:26 +0300

    1/20/2026

--- python/list.py
+++ python/list.py
@@ -23,14 +23,37 @@ motorcycle[0] = 'bugati'
 motorcycle.append('rolce royce')
 #print(motorcycle)
 
-
-
-
-
 motorcycle =[]
 
 motorcycle.append('honda')
 motorcycle.append('ducati')
 motorcycle.append('suzuki')
 
+
+
+#insert element at anywhere you want 
+
+motorcycle.insert(1,"bugati")
+
+
+
+
+#del statement 
+
+# pop_motorcyle = motorcycle.pop(0)
+# print(pop_motorcyle)
+# print(motorcycle)
+
+# print (motorcycle[0])
+
+
+
+#remove a value you dont know the position but know the value 
+
+
+motorcycle.remove('bugati')
 print(motorcycle)
+
+
+
+
