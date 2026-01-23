bb471609adfa6d35e6d57498e691902067b69ec7
meti2626 <metseboka@gmail.com>
2026-01-23 10:56:39 +0300

    1/23/2026

---
+++ python/Tuples.py
@@ -0,0 +1,10 @@
+
+dimension = (200,50)
+
+print(dimension[1])
+
+# dimension[0] =239
+# print(dimension[0])
+
+for dim in dimension:
+  print(dim)
--- python/exersices/Q4_3.py
+++ python/exersices/Q4_3.py
@@ -12,12 +12,42 @@
 # print(cube)
 
 
-players = ['charles', 'martina', 'michael', 'florence', 'eli']
+# players = ['charles', 'martina', 'michael', 'florence', 'eli']
 
-print("Here are the first three players on my team:")
+# print("Here are the first three players on my team:")
 
-for player in players[:3]:
-  print(player.title())
+# for player in players[:3]:
+#   print(player.title())
+
+
+
+my_food =  ['pizza', 'falafel', 'carrot cake']
+
+friend_food = my_food[:]
+
+my_food.append('burgers')
+friend_food.append('cucumber')
+
+print("My fav are:")
+print(my_food)
+
+print("\nMy friend's favorite foods are:")
+print(friend_food)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
