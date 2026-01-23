57c5cb92c68246eb4fdbfbd0f2799372c745ac17
meti2626 <metseboka@gmail.com>
2026-01-23 11:17:35 +0300

    1/23/2026

--- python/Tuples.py
+++ python/Tuples.py
@@ -1,10 +1,13 @@
 
 dimension = (200,50)
 
-print(dimension[1])
+print(dimension)
 
 # dimension[0] =239
 # print(dimension[0])
 
-for dim in dimension:
-  print(dim)
+# for dim in dimension:
+#   print(dim)
+
+dimension = (300,67)
+print (dimension)
