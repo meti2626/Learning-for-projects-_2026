26e71d4a30b4b0468a49b3f4e4db673bda7fa2a3
meti2626 <metseboka@gmail.com>
2026-01-23 22:24:58 +0300

    1/23/2026

--- python/ConditionalStatement.py
+++ python/ConditionalStatement.py
@@ -80,10 +80,48 @@
 fruits = ['banana' , 'avocado' ,'apple']
 
 
-if 'banana'  in  fruits:
-  print("really like bananas!")
-if 'avocado' in fruits:
-    print("really like avocado!")
+# if 'banana'  in  fruits:
+#   print("really like bananas!")
+# if 'avocado' in fruits:
+#     print("really like avocado!")
 
-if 'strawberrry'  in  fruits:
-  print("really like bananas!")    
+# if 'strawberrry'  in  fruits:
+#   print("really like bananas!")    
+
+
+
+# requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+
+
+# for requested_topping in requested_toppings:
+#   if requested_topping == 'green peppers':
+#       print("Sorry, we are out of green peppers right now.")
+#   else:
+#       print(f"Adding {requested_topping}.")
+
+# print("\nFinished making your pizza")
+
+
+# requested_toppings = ['pepper']
+
+# # dont et the if part
+# if requested_toppings:
+#   for requested_topping in requested_toppings:
+#     print(f"Adding {requested_topping}.")
+#     print("\nFinished making your pizza")
+# else:
+#        print("Are you sure you want a plain pizza?")
+
+
+
+available_toppings = ['mushrooms', 'olives', 'green peppers', 'pepperoni', 'pineapple', 'extra cheese']
+
+requested_toppings = ['mushrooms', 'french fries', 'extra cheese']
+
+for requested_topping in requested_toppings:
+   if requested_topping in available_toppings:
+      print(f"Adding {requested_topping}.")
+   else:
+     print(f"Sorry, we don't have {requested_topping}.")
+
+print("\nFinished making your pizza")     
