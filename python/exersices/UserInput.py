8b1c882e7f815fc28379724aacd71e9b910c385c
meti2626 <metseboka@gmail.com>
2026-01-25 10:04:56 +0300

    1/25/2026

--- python/exersices/UserInput.py
+++ python/exersices/UserInput.py
@@ -34,13 +34,58 @@
 # print(13%3)
 
 
-number = input("Enter a number, and I'll tell you if it's even or odd:")
-number = int(number)
+# number = input("Enter a number, and I'll tell you if it's even or odd:")
+# number = int(number)
 
 
 
-if number % 2 == 0:
-    print(f"\nThe number {number} is even.")
+# if number % 2 == 0:
+#     print(f"\nThe number {number} is even.")
 
-else:
-    print(f"\nThe number {number} is odd.")
+# else:
+#     print(f"\nThe number {number} is odd.")
+
+
+#----------------   while Loop---------#
+
+# current_number = 1
+# while current_number <= 5:
+#   print(current_number)
+#   current_number +=1
+
+
+# prompt =  "\nTell me something, and I will repeat it back to you:"
+
+# prompt += "\nEnter 'quit' to end the program. "
+
+# message = ""
+
+# while message !='quit':
+#   message = input(prompt)
+  
+#   if message !='quit':
+#      print(message)
+     
+  
+
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program. "
+
+# active = True 
+# while active :
+#   message = input(prompt)
+
+#   if message == 'quit':
+#     active =  False
+#   else :
+#     print(message)
+
+
+while True:
+  city= input(prompt)
+
+  if city == 'quit':
+    break
+  else:
+   print(f"I'd love to go to {city.title()}!")
+ 
