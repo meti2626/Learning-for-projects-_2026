7a5ba10227010e04519f0c56edf5c81d125adc17
meti2626 <metseboka@gmail.com>
2026-01-25 21:21:16 +0300

    1/25/2026-in and out

--- python/exersices/UserInput.py
+++ python/exersices/UserInput.py
@@ -81,11 +81,79 @@ prompt += "\nEnter 'quit' to end the program. "
 #     print(message)
 
 
-while True:
-  city= input(prompt)
+# while True:
+#   city= input(prompt)
 
-  if city == 'quit':
-    break
-  else:
-   print(f"I'd love to go to {city.title()}!")
+#   if city == 'quit':
+#     break
+#   else:
+#    print(f"I'd love to go to {city.title()}!")
  
+
+
+
+# current_number = 0
+
+# while current_number <10:
+#   current_number +=1
+#   if current_number % 2 ==0:
+#     continue
+#   print(current_number)
+
+
+
+# x =1 
+# while x <=5:
+#  print(x)
+#  x+=1
+ 
+
+# unconfirmed_users  = ['alice', 'brian', 'candace']
+# confirmed_users = []
+
+
+# while unconfirmed_users:
+#     current_user = unconfirmed_users.pop()
+
+#     print(f"Verifying user: {current_user.title()}")
+#     confirmed_users.append(current_user)
+
+# print("\nThe following users have been confirmed:")
+# for confirmed_user in confirmed_users:
+#     print(confirmed_user.title())
+
+
+# Removing all instances of specific values from a list
+
+
+# pets = ['dog', 'cat', 'dog', 'goldfish', 'cat', 'rabbit', 'cat']
+# print(pets)
+
+# while 'cat' in pets:
+#     pets.remove('cat')
+
+# print(pets)
+
+#Filling a dictionary with user input
+
+responses = {}
+
+polling_active = True 
+
+
+while polling_active: 
+  name = input("\nWhat is your name? ")
+  response = input("Which mountain would you like to climb someday? ")
+
+
+  responses[name] = response
+
+
+  repeat = input("Would you like to let another person respond? (yes/ no)")
+
+  if repeat == 'no':
+    polling_active = False
+
+
+print("\n--- Poll Results ---")
+print(responses)   
---
+++ python/function.py
@@ -0,0 +1,5 @@
+def greet_user(usename):
+  print(f"hello!{usename.title()}")
+
+
+greet_user('jesse')  
