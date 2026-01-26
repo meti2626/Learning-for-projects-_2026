e3014aab8eb4bfa6cc97c2bda1c9195891f7f09e
meti2626 <metseboka@gmail.com>
2026-01-26 09:45:49 +0300

    1/26/2026-second commit

--- python/function.py
+++ python/function.py
@@ -4,8 +4,6 @@
 #greet_user('jesse')  # jeese is an argument - passed from function call to function
 #greet_user('sarah')  
 
-
-
 # def get_formatted_name(first_name,last_name,middle_name=""):
 #   full_name = f"{first_name} {middle_name} {last_name}"
 #   return full_name.title()
@@ -36,19 +34,79 @@
 
 #--------Using a Function with a while Loop------------#
 
-def get_formatted_name(first_name, last_name):
+# def get_formatted_name(first_name, last_name):
 
- full_name = f"{first_name} {last_name}"
- return full_name.title()
+#  full_name = f"{first_name} {last_name}"
+#  return full_name.title()
 
 
 
   # This is an infinite loop
 
-while True:
-   print("\nPlease tell me your name:")
-   f_name = input("First nam: ")
-   l_name = input("Last name : ")
+# while True:
+#    print("\nPlease tell me your name:")
+#    print("(enter 'q' at any time to quit)")
 
-   formatted_name = get_formatted_name(f_name , l_name)
-   print(f"\nHello, {formatted_name}")
+
+#    f_name = input("First nam: ")
+#    if f_name == 'q':
+#         break
+#    l_name = input("Last name : ")
+#    if l_name == 'q':
+#        break
+   
+#    formatted_name = get_formatted_name(f_name , l_name)
+#    print(f"\nHello, {formatted_name}")
+
+
+
+
+#------Passing A loop-----#
+
+# def greet_users(names):
+
+#    for name in names:
+#       msg = f"Hello, {name.title()}"
+#       print(msg)
+
+# usernames = ['hannah','ty','margot']
+
+# greet_users(usernames)
+
+#---- Modifying a List in a function
+
+
+
+# unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
+# completed_models = []
+
+# while unprinted_designs:
+#     current_design = unprinted_designs.pop()
+#     print(f"Printing model: {current_design}")
+#     completed_models.append(current_design)
+
+# print("\nThe following models have been printed:")   
+
+# for completed_model in  completed_models:
+#     print(completed_model)
+
+
+def print_models(unprinted_designs, completed_models):
+   
+   while unprinted_designs:
+    current_design = unprinted_designs.pop()
+    print(f"Printing model: {current_design}")
+    completed_models.append(current_design)
+
+def show_completed_models(completed_models):
+
+  print("\nThe following models have been printed:")
+  for completed_model in completed_models:
+     print(completed_model)
+
+unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
+completed_models = [] 
+
+
+print_models(unprinted_designs, completed_models)
+show_completed_models(completed_models)
