b28444c1abf1cb2fa287af816785b5f4b816df21
meti2626 <metseboka@gmail.com>
2026-02-05 13:38:34 +0300

    2/5/2026

--- python/file_reader.py
+++ python/file_reader.py
@@ -1,8 +1,8 @@
-with open('pi_digits.txt') as file_object:  
+with open('name.txt') as file_object:  
    # to do work with file you have toopen it first hence - comes open() - need the file you want to open as an argument
    contents = file_object.read()
 
-print(contents)  
+print(contents.rstrip())  
 
 # rstrip() method removes any whitespace characters from the right side of a string
 
