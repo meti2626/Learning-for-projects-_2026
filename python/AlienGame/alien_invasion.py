47dfb348c92ed9b051b767a2172d3d5e7e9dbae9
meti2626 <metseboka@gmail.com>
2026-02-17 23:02:20 +0300

    2/17/2026

--- python/AlienGame/alien_invasion.py
+++ python/AlienGame/alien_invasion.py
@@ -9,7 +9,6 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
         self.bg_color = (230 , 230 , 230)
 
-
     def run_game(self):
         while True:
             for event in pygame.event.get():
