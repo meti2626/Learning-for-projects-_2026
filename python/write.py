5a3bbfd0052ec98258c527ddee23070fafab2ca3
meti2626 <metseboka@gmail.com>
2026-02-19 20:33:31 +0300

    2/19/2026#commit2

--- python/write.py
+++ python/write.py
@@ -1,8 +1,7 @@
-
 filename =  'python\\exersices\\pythonlearning.txt'
 
-
-
-
-with open(filename, 'w') as file_object:
-    file_object.write("I love programing")
+with open(filename, 'a') as file_object:
+    file_object.write("I love programing\n")
+    file_object.write(" love creating games\n ")
+    file_object.write("I also love finding meaning in large datasets.\n")
+    file_object.write("I love creating apps that can run in a browser.\n")
