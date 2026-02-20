2a089d498309654789c388af4986db17ad717d99
meti2626 <metseboka@gmail.com>
2026-02-21 01:56:07 +0300

    2/21/2026#commit#1

---
+++ python/alice.py
@@ -0,0 +1,14 @@
+
+# filename = 'alice.txt'
+# try:
+
+#  with open(filename, encoding = 'utf-8') as f:
+#   contents = f.read()
+# except FileNotFoundError:
+#     print(f"Sorry, the file {filename} does not exist.")
+
+
+title = "Alice in Wonderland"
+  
+print(title.split())
+   
